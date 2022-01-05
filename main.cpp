6d657825db467df2098cf49dd2c9186be27b93f2
abiniaz1 <60835547+abiniaz1@users.noreply.github.com>
2022-01-05 18:11:17 -0500

    Update and rename proj2.cpp to main.cpp

---
+++ main.cpp
@@ -0,0 +1,26 @@
+﻿// File: proj2.cpp
+// Author: Abid Niaz
+// Date: 3-19-18
+// Description: Runs the dragon game where a player can hatch and fight dragons
+//              with dragons.
+
+
+#include "Dragon.h"
+#include "Game.h"
+#include "Extras.h"
+
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <vector>
+
+
+using namespace std;
+
+
+int main()
+{
+	Game newGame;
+	newGame.StartGame();
+    return 0;
+}
